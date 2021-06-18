ea0b5cdbb64437e04838207803e989153fe01803
Brayden England <bray.patriot@gmail.com>
2021-06-18 15:15:42 -0600

    Different colors for Tabs1

--- src/components/Tabs1/Tabs1.js
+++ src/components/Tabs1/Tabs1.js
@@ -4,11 +4,12 @@ import { useState } from 'react'
 const Tabs1 = ({tabs, colorType}) => {
 
   const [selected, setSelected] = useState(0)
+  let tabClass = 'tab-item selected-' + colorType
 
   return (
     <div className="Tabs1">
       {tabs.map((tab, index) => (
-        <div className={selected === index ? 'tab-item selected' : 'tab-item'} onClick={() => setSelected(index)}>{tab}</div>
+        <div className={selected === index ? `tab-item selected-${colorType} selected` : `tab-item`} onClick={() => setSelected(index)}>{tab}</div>
       ))}
     </div>
   )
--- src/components/Tabs1/Tabs1.stories.js
+++ src/components/Tabs1/Tabs1.stories.js
@@ -13,12 +13,36 @@ export default {
 // storybook creates 'template' of how args map to renderings
 const Template = args => <Tabs1 {...args} />
 
-export const LinePrimary = Template.bind({})
-LinePrimary.args = {
+export const BlackTabs = Template.bind({})
+BlackTabs.args = {
   tabs: ['Tab 1', 'Tab 2', 'Tab 3'],
   colorType: 'black'
 }
-LinePrimary.parameters = {
+BlackTabs.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/ew4XdbdPViYsWSV8BIR5M1/Untitled?node-id=0%3A1'
+  }
+}
+
+export const BlueTabs = Template.bind({})
+BlueTabs.args = {
+  tabs: ['Home', 'About Us', 'Contact'],
+  colorType: 'blue'
+}
+BlueTabs.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/ew4XdbdPViYsWSV8BIR5M1/Untitled?node-id=0%3A1'
+  }
+}
+
+export const GreenTabs = Template.bind({})
+GreenTabs.args = {
+  tabs: ['Home', 'About Us', 'Contact'],
+  colorType: 'green'
+}
+GreenTabs.parameters = {
   design: {
     type: 'figma',
     url: 'https://www.figma.com/file/ew4XdbdPViYsWSV8BIR5M1/Untitled?node-id=0%3A1'
